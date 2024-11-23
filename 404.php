ca64772e3ef29f1f73240ad6f3b763346118f7e3
kpietryga <krzysztof.pietryga@gmail.com>
2024-11-23 23:35:20 +0100

    Add 404

---
+++ 404.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+
+<main>
+    <h1>Error 404</h1>
+    <p>Sorry, but the page you are looking for does not exist.</p>
+    <a href="<?php echo home_url(); ?>">Return to the homepage</a>
+</main>
+
+<?php get_footer(); ?>
