5f8b055cc01cda0fe7979664425846f4ed63e25f
kpietryga <krzysztof.pietryga@gmail.com>
2024-11-23 23:40:47 +0100

    Add bootstrap-css

--- functions.php
+++ functions.php
@@ -2,6 +2,7 @@
 function my_theme_load_styles() {
     wp_enqueue_style('my-style', get_stylesheet_uri());
 }
+
 add_action('wp_enqueue_scripts', 'my_theme_load_styles');
 
 function my_theme_register_menu() {
@@ -13,3 +14,12 @@ function my_theme_functions() {
     add_theme_support('post-thumbnails');
 }
 add_action('after_setup_theme', 'my_theme_functions');
+
+function my_theme_enqueue_styles() {
+    wp_enqueue_style('bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css');
+
+    wp_enqueue_style('my-theme-style', get_stylesheet_uri());
+
+    wp_enqueue_script('bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js', array(), null, true);
+}
+add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
